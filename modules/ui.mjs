0da1733517801cba06521d2aa7a22c22cc4c1e26
Wasit Faraizi <wasit.allin1@gmail.com>
2022-04-13 13:35:26 +0600

    Initial commit

---
+++ modules/ui.mjs
@@ -0,0 +1,88 @@
+import convertKeyToInt from './keyboard.mjs';
+
+export const UISelectors = {
+  card: document.querySelector('.card'),
+  keyboard: document.querySelector('.keyboard'),
+  text: document.querySelector('.text'),
+  typingField: document.querySelector('#typing-field'),
+  spacebar: document.querySelector('.spacebar'),
+  keys: document.querySelectorAll('td'),
+  playBtn: document.querySelector('.play-btn'),
+  settingsBtn: document.querySelector('.settings-btn'),
+  settingsState: document.querySelector('.settings-state'),
+  wpm: document.querySelector('.wpm h2'),
+  accuracy: document.querySelector('.accuracy h2'),
+  time: document.querySelector('.time h2'),
+  finishState: document.querySelector('.finish-state')
+};
+
+export default function showText(words) {
+  const wordsArr = words.join(' ');
+  UISelectors.text.innerHTML = wordsArr;
+}
+
+export function blurText() {
+  UISelectors.text.style.color = 'transparent';
+  UISelectors.text.style.textShadow = '0 0 12px #666';
+}
+
+export function unblurText() {
+  UISelectors.text.style.color = '#dbdbdb';
+  UISelectors.text.style.textShadow = 'none';
+}
+
+export function animateSpacebar() {
+  UISelectors.spacebar.style.backgroundColor = '#007acc';
+  UISelectors.spacebar.style.opacity = '0.7';
+
+  setTimeout(() => {
+    UISelectors.spacebar.style.backgroundColor = 'transparent';
+    UISelectors.spacebar.style.opacity = '1';
+  }, 100);
+}
+
+export function animateRandKey(pressedKey) {
+  const keys = Array.from(UISelectors.keys);
+  const key = keys[convertKeyToInt(pressedKey)];
+
+  key.style.backgroundColor = '#007acc';
+  key.style.opacity = '0.7';
+  setTimeout(() => {
+    key.style.backgroundColor = 'transparent';
+    key.style.opacity = '1';
+  }, 100);
+}
+
+export function clearTypingField() {
+  UISelectors.typingField.value = null;
+}
+
+export function changeStateToSettings() {
+  UISelectors.card.style.display = 'none';
+  UISelectors.keyboard.style.display = 'none';
+  UISelectors.spacebar.style.display = 'none';
+  UISelectors.finishState.style.display = 'none';
+
+  UISelectors.settingsState.style.display = 'block';
+}
+
+export function changeStateToFinish(wpm, accuracy, time) {
+  UISelectors.wpm.innerHTML = `${wpm} WPM`;
+  UISelectors.accuracy.innerHTML = `${accuracy}%`;
+  UISelectors.time.innerHTML = `${time}`;
+
+  UISelectors.card.style.display = 'none';
+  UISelectors.keyboard.style.display = 'none';
+  UISelectors.spacebar.style.display = 'none';
+
+  UISelectors.finishState.style.opacity = 1;
+  setTimeout(() => {
+    UISelectors.wpm.style.opacity = 1;
+  }, 300);
+  setTimeout(() => {
+    UISelectors.accuracy.style.opacity = 1;
+  }, 600);
+  setTimeout(() => {
+    UISelectors.time.style.opacity = 1;
+  }, 900);
+}
---
+++ script.js
@@ -0,0 +1,95 @@
+import generateText from './modules/text.mjs';
+import showText from './modules/ui.mjs';
+import {
+  time,
+  startTimer,
+  stopTimer,
+  resetTimer,
+  stoptime
+} from './modules/timer.mjs';
+import {
+  UISelectors,
+  blurText,
+  unblurText,
+  animateSpacebar,
+  animateRandKey,
+  clearTypingField,
+  changeStateToSettings,
+  changeStateToFinish
+} from './modules/ui.mjs';
+
+// Variables
+const wordsArr = generateText(50);
+let count = 0;
+let correctChars = 0;
+let wrongChars = 0;
+let currentWord = wordsArr[0];
+let wpm = 0;
+let accuracy = 0;
+
+// Load content
+function loadContent() {
+  showText(wordsArr);
+  UISelectors.typingField.focus();
+}
+
+// App
+function app(e) {
+  if (e.key === 'Tab') {
+    e.preventDefault();
+
+    UISelectors.playBtn.focus();
+  }
+
+  if (UISelectors.typingField === document.activeElement) {
+    // Keyboard animation
+    try {
+      if (e.key === ' ') {
+        animateSpacebar();
+      } else {
+        animateRandKey(e.key);
+      }
+    } catch (error) {}
+
+    // Start Timer
+    if (stoptime === true) {
+      startTimer();
+    }
+
+    if (e.key === ' ') {
+      if (UISelectors.typingField.value.trim() === currentWord) {
+        correctChars += currentWord.length + 1;
+      } else {
+        wrongChars += currentWord.length;
+      }
+
+      // Clear typing field
+      clearTypingField();
+
+      count = count + 1;
+      currentWord = wordsArr[count];
+
+      if (count === wordsArr.length) {
+        // Stop timer
+        stopTimer();
+
+        // Calculate data
+        const totalChars = correctChars + wrongChars;
+        accuracy = Math.round((correctChars / totalChars) * 100);
+        wpm = Math.round(correctChars / 5 / (time / 60));
+
+        changeStateToFinish(wpm, accuracy, time);
+
+        resetTimer();
+      }
+      e.preventDefault();
+    }
+  }
+}
+
+// Event listeners
+window.addEventListener('DOMContentLoaded', loadContent);
+window.addEventListener('keydown', app);
+UISelectors.typingField.addEventListener('focusin', unblurText);
+UISelectors.typingField.addEventListener('focusout', blurText);
+UISelectors.settingsBtn.addEventListener('click', changeStateToSettings);
